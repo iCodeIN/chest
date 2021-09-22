dbb1fb248ad88b80b45b09a5e63417eb0b40de37
Mathew Horner <mathewhorner456@gmail.com>
2021-09-21 22:29:58 -0500

    Added ability to send GET requests and fixed string parsing.

--- server/src/command.rs
+++ server/src/command.rs
@@ -2,17 +2,27 @@ use crate::errors::ParseCommandError;
 use crate::overrides::Result;
 
 pub enum Command {
-    Add { key: String, value: String }
+    Add { key: String, value: String },
+    Get { key: String }
 }
 
 impl Command {
+    /// Parses the given string into a Command type.
     pub fn parse(input: &String) -> Result<Command> {
         let tokens = input.split(" ").collect::<Vec<&str>>();
-        if tokens.len() != 3 {
-            return Err(Box::new(ParseCommandError::MalformedInput));
-        }
         return match tokens[0] {
-            "ADD" => Ok(Command::Add { key: tokens[1].into(), value: tokens[2].into() }),
+            "ADD" => {
+                if tokens.len() != 3 {
+                    return Err(Box::new(ParseCommandError::MalformedInput));
+                }
+                Ok(Command::Add { key: tokens[1].into(), value: tokens[2].into() })
+            },
+            "GET" => {
+                if tokens.len() != 2 {
+                    return Err(Box::new(ParseCommandError::MalformedInput));
+                }
+                Ok(Command::Get { key: tokens[1].into() })
+            },
             _ => Err(Box::new(ParseCommandError::InvalidCommand { command: tokens[0].into() })),
         }
     }
--- server/src/server.rs
+++ server/src/server.rs
@@ -3,7 +3,6 @@ use crate::overrides::Result;
 use std::collections::HashMap;
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
-use std::str;
 
 pub struct Server {
     storage: HashMap<String, String>,
@@ -14,10 +13,11 @@ impl Server {
         Self { storage: HashMap::new() }
     }
 
+    /// Starts the server, and begins listening for connections on the given port.
     pub fn listen(&mut self, port: u16) -> Result<()> {
         let listener = TcpListener::bind(format!("127.0.0.1:{}", port))?;
         for stream in listener.incoming() {
-            match self.handle_client(&mut stream?) {
+            match self.handle_request(&mut stream?) {
                 Ok(()) => (),
                 // TODO: Add some additional logging? Find a way to send an error to the user?
                 Err(err) => {
@@ -28,17 +28,31 @@ impl Server {
         Ok(())
     }
 
-    fn handle_client(&mut self, stream: &mut TcpStream) -> Result<()> {
+    /// Handles each request to the server.
+    fn handle_request(&mut self, stream: &mut TcpStream) -> Result<()> {
         let mut input = [0; 4096];
         stream.read(&mut input)?;
-        match Command::parse(&String::from(str::from_utf8(&input)?))? {
+
+        // TODO: Don't do unwrap here?
+        let end = input.iter().position(|x| *x == 0).unwrap();
+        let mut input = String::from(std::str::from_utf8(&input[0..end])?);
+        input.pop();
+
+        match Command::parse(&input)? {
             Command::Add { key, value } => {
                 match self.storage.insert(key, value) {
                     Some(_) => stream.write("0 Updated".as_bytes())?,
                     None => stream.write("0 Added".as_bytes())?,
                 }
+            },
+            Command::Get { key } => {
+                match self.storage.get(&key) {
+                    Some(value) => stream.write(format!("0 {}", value).as_bytes())?,
+                    None => stream.write("2 Not Found".as_bytes())?,
+                }
             }
         };
+
         Ok(())
     }
 }
